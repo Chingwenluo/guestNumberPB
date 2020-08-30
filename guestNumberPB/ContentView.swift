aa2d61c0d0cda5de48c4d8fb67e64ed487d0419e
ChingWen <fsvnxwx@gmail.com>
2020-08-30 23:41:05 +0800

    Initial Commit

---
+++ guestNumberPB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guestNumberPB
+//
+//  Created by eric on 2020/8/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ guestNumberPB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guestNumberPB
+//
+//  Created by eric on 2020/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
